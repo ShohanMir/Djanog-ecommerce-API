ee4a8c57389473cc2d7c3eb79f38593b7089fe8c
Shohan Mir <shohan7mir@gmail.com>
2023-04-10 07:04:15 +0600

    user form fix

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
 
 from django import forms
 
@@ -8,17 +9,25 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         
-    def __ini__(self, *args, **kwargs):
-        super(CreateUserForm).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
         
         self.fields['email'].required = True
 
-    #email validation
+    # email validation
     def clean_email(self):
         email = self.cleaned_data.get('email')
         
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('The email is invalid')
+            raise forms.ValidationError('This email is already registered')
         if len(email) >= 350:
             raise forms.ValidationError('Your email is too long!')
+        
+         # validate email format
+        validator = EmailValidator('Enter a valid email address.')
+        try:
+            validator(email)
+        except forms.ValidationError:
+            raise forms.ValidationError('Enter a valid email address.')
+
         return email
--- account/views.py
+++ account/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponseBadRequest
+# from django.http import HttpResponseBadRequest
 
 from .forms import CreateUserForm
 
 # Create your views here.
 
 def register(request):
+    # if request.method != 'POST':
+    #     return HttpResponseBadRequest('Bad Request')
     form = CreateUserForm()
-    
+
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
-        if form.isValid():
+        if form.is_valid():
             form.save()
             return redirect('store')
+        else:
+            # re-render the form with error messages
+            context = {'form': form}
+            return render(request, 'account/registration/register.html', context=context)   
         
     context = {'form': form}
     return render(request, 'account/registration/register.html', context=context)
+
+
+
 
 def email_verification(request):
     pass
