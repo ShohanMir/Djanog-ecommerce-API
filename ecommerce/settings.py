950b5ace5d3fd200308cbdea19fcf6fdef6b40d8
Shohan Mir <shohan7mir@gmail.com>
2023-04-12 07:01:43 +0600

    email verification and smtp setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,3 +138,12 @@ MEDIA_ROOT= BASE_DIR / 'static/media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#SMTP configuration
+EMAIL_BACKEND = '   django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAOIL_HOST_PASSWORD = 'your-password'
